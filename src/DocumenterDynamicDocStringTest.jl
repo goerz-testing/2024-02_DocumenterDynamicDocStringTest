13d7c87e899b4bc5a738919f30dbc52584ec435d
Michael Goerz <mail@michaelgoerz.net>
2024-02-19 16:29:42 -0500

    Add `MyType` with dynamic documentation

--- src/DocumenterDynamicDocStringTest.jl
+++ src/DocumenterDynamicDocStringTest.jl
@@ -1,5 +1,17 @@
 module DocumenterDynamicDocStringTest
 
-# Write your package code here.
+struct MyType
+    value::Int
+end
+
+"""A type with a static documentation"""
+struct MyType2
+    value::Int
+end
+
+Docs.getdoc(t::MyType) = "Documentation for MyType with value $(t.value)"
+
+x = MyType(1)
+y = MyType(2)
 
 end
